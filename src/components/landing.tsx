d5faf2eebadbe95e7859d3eaaecf89e62ef1a5cb
Emma Cazalye <e.cazaly7@gmail.com>
2020-06-25 21:23:14 +1000

    Add text for landing sections

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -13,27 +13,31 @@ class Landing extends Component {
                         <h2>We make cold-pressed junk free juice</h2>
                     </div>
                 </div>
+                <div className="sectionDivider"/>
                 <LandingSections
                     Title1="WE ARE"
                     Title2="LOCAL"
                     Description="We’re a Tasmanian owned and operated small business, making junk-free cold pressed juices from locally sourced fruit and vegetables"
-                    Image="/img/logo.jpg"
+                    Image="/img/about.jpg" 
                     alt="The Juice Press team sitting on an apple crate"
                 />
-
-                {/* <div id="about">
-                    <h1>We are local</h1>
-                </div>
-
-                <div id="enviro">
-                    <h1>We care about the environment</h1>
-                </div>
-                <div id="visit">
-                    <h1>Come visit us</h1>
-                    </div>  
-                <div id="shop">
-                    <h1>SHOP</h1>
-                </div> */}
+                <LandingSections
+                    Title1="WE CARE ABOUT"
+                    Title2="THE ENVIRONMENT"
+                    Description="We’re all about sustainability and minimising waste
+                    Our juices are packaged in glass bottles- return yours for a discount on future purchases"
+                    Image="/img/bottles.jpg" 
+                    alt="Glass bottles full of different juices"
+                />
+                <LandingSections
+                    Title1="COME"
+                    Title2="VISIT US"
+                    Description="We’re at the Farm Gate Market on Bathurst st. in Hobart every Sunday from 8.30 am - 12.30
+                    We’d love to see you!"
+                    Image="/img/Market.jpg" 
+                    alt="Tasmanian Juice Press at the Farm Gate Market"
+                />
+                <div className="sectionDivider"/>
             </div>
         )
     }
