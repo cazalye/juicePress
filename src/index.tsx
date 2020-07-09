aed09c458c9ff08a00b1cd0b40a9bd58211aa7b2
Emma Cazalye <e.cazaly7@gmail.com>
2020-07-09 10:58:19 +1000

    Add links for react-mdl / materialise

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import 'react-mdl/extra/material.js';
+import 'react-mdl/extra/material.css';  
+  
 
 ReactDOM.render(
   <BrowserRouter>
