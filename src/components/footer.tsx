18d3b8265629b989a2a8769d7cb3b441651759d3
Emma Cazalye <e.cazaly7@gmail.com>
2020-07-13 15:25:34 +1000

    Add correct links to footer and style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,17 +10,19 @@ class FooterDetail extends Component{
             <FooterSection type="bottom">
                 <FooterDropDownSection title="Shop Juices">
                     <FooterLinkList>
-                        <a href="#">750ml Juice Blends</a>
-                        <a href="#">2L Juice Blends</a>
-                        <a href="#">Weekly Special</a>
-                        <a href="#">Immune Boost Packs</a>
+                        <a href="https://brandsonparade.com/product/tasmanian-juice-press-subscription" rel="noopener noreferrer" target="_blank">Juice Subscription</a>
+                        <a href="https://brandsonparade.com/product/tasmanian-juice-press-cold-pressed-weekly-juice-special-750ml/" rel="noopener noreferrer" target="_blank">Weekly Special</a>
+                        <a href="https://brandsonparade.com/product-category/juice-pack/" rel="noopener noreferrer" target="_blank">Juice Packs</a>
+                        <a href="https://brandsonparade.com/product-category/the-tasmanian-juice-press/immune-boost/" rel="noopener noreferrer" target="_blank">Immune Boost Packs</a>
+                        <a href="https://brandsonparade.com/product-category/the-tasmanian-juice-press/750ml-juice-bottles/" rel="noopener noreferrer" target="_blank">750ml Juice Blends</a>
+                        <a href="https://brandsonparade.com/product-category/the-tasmanian-juice-press/2l-juice-bottles/" rel="noopener noreferrer" target="_blank">2L Juice Blends</a>
                     </FooterLinkList>
                 </FooterDropDownSection>
                 <FooterDropDownSection title="About Us">
                     <FooterLinkList>
-                        <a href="#">Who Are We</a>
-                        <a href="#">Environmental Impact</a>
-                        <a href="#">Visit Us</a>
+                        <a href="/#about">Who Are We</a>
+                        <a href="/#enviro">Environmental Impact</a>
+                        <a href="/#visitUs">Visit Us</a>
                     </FooterLinkList>
                 </FooterDropDownSection>
                 <FooterDropDownSection title="Contact Us">
