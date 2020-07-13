40448955dfc3f4f9c1a58897c45aa431526f0e67
Emma Cazalye <e.cazaly7@gmail.com>
2020-07-13 15:06:13 +1000

    Style shop component on landing page

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import "./landing.scss";
 import LandingSections from "./landingSections";
 import FooterDetail from './footer';
+import Shop from './shop';
 
 class Landing extends Component {
     render() {
@@ -43,8 +44,49 @@ class Landing extends Component {
                     alt="Tasmanian Juice Press at the Farm Gate Market"
                 />
                 
-                
                 <a id="shop"/>
+                <div id="shop-section">
+                    <h1>SHOP JUICES</h1>
+                    <div className="shop-container">
+                        <Shop
+                        juiceTitle="Juice Subscripion"
+                        juiceImage="./img/subscription.png"
+                        alt="Bottles of juice, weekly subscription"
+                        shopLink="https://brandsonparade.com/product/tasmanian-juice-press-subscription/"
+                        />
+                        <Shop
+                        juiceTitle="Weekly Special"
+                        juiceImage="./img/special.png"
+                        alt="Weekly special juice bottle"
+                        shopLink="https://brandsonparade.com/product/tasmanian-juice-press-cold-pressed-weekly-juice-special-750ml/"
+                        />
+                        <Shop
+                        juiceTitle="Immune Boost"
+                        juiceImage="./img/immune.png"
+                        alt="Bottles of immune boost juice"
+                        shopLink="https://brandsonparade.com/product-category/the-tasmanian-juice-press/immune-boost/"
+                        />
+                        <Shop
+                        juiceTitle="Juice Packs"
+                        juiceImage="./img/pack.png"
+                        alt="Bottles of juice packs"
+                        shopLink="https://brandsonparade.com/product-category/juice-pack/"
+                        />
+                        <Shop
+                        juiceTitle="750ml Juice Blend"
+                        juiceImage="./img/750ml.png"
+                        alt="750ml juice bottles"
+                        shopLink="https://brandsonparade.com/product-category/the-tasmanian-juice-press/750ml-juice-bottles/"
+                        />
+                        <Shop
+                        juiceTitle="2L Juice Blend"
+                        juiceImage="./img/2l.png"
+                        alt="2L juice bottles"
+                        shopLink="https://brandsonparade.com/product-category/the-tasmanian-juice-press/2l-juice-bottles/"
+                        />
+                    </div>
+                </div>
+                
                 <FooterDetail/>
                 <a id="contact"/>
             </div>
