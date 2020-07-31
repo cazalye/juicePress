8b139bb60fafd7f42bb5e0b886c42b2c53dc9e22
Emma <e.cazaly7@gmail.com>
2020-07-31 12:26:16 +1000

    build

---
+++ build/precache-manifest.eb1a3a1beb4cd1abff80addb081b0da4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719373ca06b3939ed5bc403dbd5b8df3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c63987384a3807b5ea6",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "17da6bf3b7943e8b0097",
+    "url": "/static/css/main.f21f2335.chunk.css"
+  },
+  {
+    "revision": "6c63987384a3807b5ea6",
+    "url": "/static/js/2.2fa52488.chunk.js"
+  },
+  {
+    "revision": "6685e3d356dd613e987e14a768b8c215",
+    "url": "/static/js/2.2fa52488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17da6bf3b7943e8b0097",
+    "url": "/static/js/main.1f219927.chunk.js"
+  },
+  {
+    "revision": "18a9391402770cf2fcd0",
+    "url": "/static/js/runtime-main.f4219c82.js"
+  }
+]);
