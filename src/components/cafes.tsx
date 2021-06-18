5211951be90dc7834d59677ef0948b343ec599ee
Emma Cazalye <e.cazaly7@gmail.com>
2021-06-19 09:27:26 +1000

    add cafe component

---
+++ src/components/cafes.tsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+
+
+
+class Cafes extends Component{
+
+    render() {
+        return (
+            <div className="cafe">
+                    <h2 className="bar"/>
+                    <h2 className="title1-cafe">Look for </h2>
+                    <h2 className="title2-cafe">our juices here</h2>
+                    <p>Look out for our juices here</p>
+            
+
+                <table>
+                    <tbody>
+                        <tr>
+                            <td><a href="https://www.abundanceonthequay.com.au/" target="_blank" rel="noreferrer noopener">Abundance</a></td>
+                            <td><a href="http://gastowneast.com.au/" target="_blank" rel="noreferrer noopener">Gastown East</a></td>
+                            <td><a href="https://www.liveatprovidence.com.au/" target="_blank" rel="noreferrer noopener">Providence</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.fullersbookshop.com.au/cafe/" target="_blank" rel="noreferrer noopener">Afterword Cafe</a></td>
+                            <td><a href="https://m.facebook.com/gingerbrownhobart/" target="_blank" rel="noreferrer noopener">Ginger Brown</a></td>
+                            <td><a href="https://www.punchandladle.com.au/" target="_blank" rel="noreferrer noopener">Punch &amp; Ladle</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.alabamahobart.com.au/" target="_blank" rel="noreferrer noopener">Alabama Hotel</a></td>
+                            <td><a href="https://www.theglass.house/" target="_blank" rel="noreferrer noopener">Glasshouse</a></td>
+                            <td><a href="https://rainchecklounge.com.au/" target="_blank" rel="noreferrer noopener">Raincheck Lounge</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://aloftrestaurant.com/" target="_blank" rel="noreferrer noopener">Aloft</a></td>
+                            <td><a href="https://www.facebook.com/GroundsmanEspresso/" target="_blank" rel="noreferrer noopener">Groundsman Espresso</a></td>
+                            <td><a href="http://www.roomforapony.com.au/" target="_blank" rel="noreferrer noopener">Room for a Pony</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.audreycoffee.com/" target="_blank" rel="noreferrer noopener">Audrey</a></td>
+                            <td><a href="https://heavenlybiteshobart.com.au/" target="_blank" rel="noreferrer noopener">Heavenly Bites</a></td>
+                            <td><a href="http://www.rudeboyhobart.com.au/" target="_blank" rel="noreferrer noopener">Rude Boy</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.bakedglutenfree.com.au/" target="_blank" rel="noreferrer noopener">Baked</a></td>
+                            <td><a href="https://hillstreetgrocer.com/" target="_blank" rel="noreferrer noopener">Hill Street Grocer</a></td>
+                            <td><a href="https://spencershobart.com.au/" target="_blank" rel="noreferrer noopener">Spencers</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.facebook.com/bearwithmecoffeeandfood/" target="_blank" rel="noreferrer noopener">Bear With Me</a></td>
+                            <td><a href="https://m.facebook.com/Thelansdownecrescentcafe" target="_blank" rel="noreferrer noopener">Lansdowne Cafe</a></td>
+                            <td><a href="https://www.facebook.com/staplecoffeehobart/" target="_blank" rel="noreferrer noopener">Staple Coffee</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.theblackfootedpig.com.au/" target="_blank" rel="noreferrer noopener">Black Footed Pig</a></td>
+                            <td><a href="https://www.facebook.com/littlelotuscafe/" target="_blank" rel="noreferrer noopener">Little Lotus Cafe</a></td>
+                            <td><a href="https://stockmarketrestaurant.com.au/" target="_blank" rel="noreferrer noopener">Stock Market</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.facebook.com/bohoathebeach/" target="_blank" rel="noreferrer noopener">Boho At The Beach</a></td>
+                            <td><a href="https://www.facebook.com/LIZZIEANDLEFROY/" target="_blank" rel="noreferrer noopener">Lizzie &amp; Lefroy</a></td>
+                            <td><a href="https://www.straightupcoffeeandfood.com.au/" target="_blank" rel="noreferrer noopener">Straight Up</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.borninbrunswick.com.au/" target="_blank" rel="noreferrer noopener">Born In Brunswick</a></td>
+                            <td>Local Grocer</td>
+                            <td><a href="https://www.facebook.com/sunbearhobart/" target="_blank" rel="noreferrer noopener">Sunbear</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.thebrickfactory.com.au/" target="_blank" rel="noreferrer noopener">Brick Factory</a></td>
+                            <td><a href="https://www.facebook.com/Machine-Laundry-Cafe-113296732063100/" target="_blank" rel="noreferrer noopener">Machine Laundry Cafe</a></td>
+                            <td><a href="https://www.thebeachhousebb.com/" target="_blank" rel="noreferrer noopener">The Beach House</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.burymestanding.com.au/" target="_blank" rel="noreferrer noopener">Bury Me Standing</a></td>
+                            <td><a href="https://www.facebook.com/macquariesoul/" target="_blank" rel="noreferrer noopener">Macquarie Soul</a></td>
+                            <td><a href="https://www.facebook.com/unpackedbulkwholefoods/" target="_blank" rel="noreferrer noopener">Unpacked</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://m.facebook.com/capulusespressowatchorn/" target="_blank" rel="noreferrer noopener">Capulus</a></td>
+                            <td><a href="https://mona.net.au/" target="_blank" rel="noreferrer noopener">Mona</a></td>
+                            <td><a href="https://urbangreek.com.au/" target="_blank" rel="noreferrer noopener">Urban Greek</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.coterieandco.com.au/" target="_blank" rel="noreferrer noopener">Coterie &amp; Co.</a></td>
+                            <td><a href="https://www.movenpick.com/en/australia-pacific/australia/hobart/" target="_blank" rel="noreferrer noopener">Movenpick Hobart</a></td>
+                            <td><a href="https://www.vegbar.com.au/" target="_blank" rel="noreferrer noopener">Veg Bar</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.facebook.com/ElevensesHobart/" target="_blank" rel="noreferrer noopener">Elevenses</a></td>
+                            <td><a href="https://www.facebook.com/ozuscoffee/" target="_blank" rel="noreferrer noopener">Ozus</a></td>
+                            <td><a href="https://www.facebook.com/viliciacoffee/" target="_blank" rel="noreferrer noopener">Vilicia</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.eumarrah.com.au/" target="_blank" rel="noreferrer noopener">Eumarrah</a></td>
+                            <td><a href="https://www.facebook.com/thepicnicbasket.com.au" target="_blank" rel="noreferrer noopener">Picnic Basket</a></td>
+                            <td><a href="https://villino.com.au/" target="_blank" rel="noreferrer noopener">Villino</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://www.ferntreetavern.com.au/" target="_blank" rel="noreferrer noopener">Fern Tree Tavern</a></td>
+                            <td><a href="https://www.pigeonholecafe.com.au/" target="_blank" rel="noreferrer noopener">Pigeon Hole</a></td>
+                            <td><a href="http://www.vitarealfood.com/" target="_blank" rel="noreferrer noopener">Vita</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="https://cafefriendsandfamily.com/" target="_blank" rel="noreferrer noopener">Friends &amp; Family</a></td>
+                            <td><a href="https://www.newnorfolkdistillery.com/shop/providore/3" target="_blank" rel="noreferrer noopener">Pop-up Providore</a></td>
+                            <td><a href="https://www.facebook.com/whiskandco/" target="_blank" rel="noreferrer noopener">Whisk &amp; Co</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
+}
+
+export default Cafes;
+
