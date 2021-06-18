8faa29775e130db5a189975d87304a9fb90fd258
Emma Cazalye <e.cazaly7@gmail.com>
2021-06-19 09:27:05 +1000

    update navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,26 +15,33 @@ class Navbar extends Component<any, any> {
         // const title = <Link to="/#home" className="header-title"> The Tasmanian Juice Press </Link> as any;
         return (
             <Layout>
+                <div className="promo-banner">            
+                    <p className="promo-text"><img className="" alt="dark mofo" src="./img/darkmofo.jfif"  width="40px"/><strong>DARK MOFO winter feast</strong>: stall 8 inside the shed  </p>
+                </div>
                 <Header title={<Link to="/#home" className="header-title">The Tasmanian Juice Press </Link>}>
                 <Link to="/#home"><img className="logo" alt="logo" src="./img/logoTransparent.png"  width="40px"/></Link>
                     <Navigation className="header-nav">
                         {/* <Link to="/#range">Juice Range</Link>  */}
                         <Link className="shop-link" to="/#shop">Shop</Link>
-                        <Link className="nav-link" to="/#about">About Us</Link> 
-                        <Link className="nav-link" to="/#visitUs">Visit Us</Link>
-                        <Link className="nav-link" to="/#enviro">Environmental Impact</Link> 
+                        <Link className="nav-link" to="/#range">Juice Range</Link>
+                        <Link className="nav-link" to="/#visitUs">Markets</Link>
+                        <Link className="nav-link" to="/#cafes">Find our Juice</Link>
+                        <Link className="nav-link" to="/#about">About</Link> 
+                        {/* <Link className="nav-link" to="/#enviro">Environmental Impact</Link>  */}
                         <Link className="nav-link" to="/#contact">Contact</Link>
                     </Navigation>
                 </Header>
                 <Drawer title={<Link onClick={() => this.hideToggle()} to="/" className="header-title">The Tasmanian Juice Press </Link> as any}>
                     <Navigation className="drawer-nav" >
-                        {/* <Link onClick={() => this.hideToggle()} to="/#range">Juice Range</Link> */}
-                        <Link className="drawer-logo" to="/#home"><img className="logo" alt="logo" src="./img/logoTransparent.png"  width="40px"/></Link>
-                        <Link className="drawer-title" onClick={() => this.hideToggle()} to="/">The Tasmanian Juice Press</Link>
+                        <Link className="drawer-logo" onClick={() => this.hideToggle()} to="/#home"><img className="logo" alt="logo" src="./img/logoTransparentDark.png"  width="40px"/></Link>
+                        {/* <Link className="drawer-title" onClick={() => this.hideToggle()} to="/">Home</Link> */}
+                        <Link onClick={() => this.hideToggle()} to="/#home">Home</Link>
                         <Link onClick={() => this.hideToggle()} to="/#shop">Shop</Link>
-                        <Link onClick={() => this.hideToggle()} to="/#about">About Us</Link>
-                        <Link onClick={() => this.hideToggle()} to="/#visitUs">Visit Us</Link>
+                        <Link onClick={() => this.hideToggle()} to="/#range">Juice Range</Link>
+                        <Link onClick={() => this.hideToggle()} to="/#visitUs">Markets</Link>
+                        <Link onClick={() => this.hideToggle()} to="/#cafes">Find our Juice</Link>
                         <Link onClick={() => this.hideToggle()} to="/#enviro">Environmental Impact</Link>
+                        <Link onClick={() => this.hideToggle()} to="/#about">About</Link>
                         <Link onClick={() => this.hideToggle()} to="/#contact">Contact</Link>
                     </Navigation>
                 </Drawer>
